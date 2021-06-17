263dbee90354683849e016f4ec39359275e7bec3
Suellen Piacenti <84042162+supiacenti@users.noreply.github.com>
2021-06-17 20:23:26 -0300

    segundo commit
    Destaque e menu de configurações.

--- Netflix/js/netflix.js
+++ Netflix/js/netflix.js
@@ -1,8 +1,34 @@
 $("#list").hide();
+
+$("#video").hide();
+var d = document.querySelector("#destaque");
+var v = document.querySelector("#video");
+var vc = document.getElementsByClassName("video");
+d.addEventListener("mouseenter", function(){
+    $("#video").show();
+    v.autoplay = true;
+    v.load();
+});
+d.addEventListener("mouseleave", function(){
+    $("#video").hide();
+    v.autoplay = false;
+    v.load();
+});
 var c = document.querySelector("#conf");
+var c2 = document.querySelector("#conf2");
 c.addEventListener("mouseenter", function(){
     $("#list").show();
 });
-c.addEventListener("mouseleave", function(){
+c.addEventListener("click", function(){
     $("#list").hide();
+});
+c2.addEventListener("mouseenter", function(){
+    $("#list").show();
+});
+c2.addEventListener("click", function(){
+    $("#list").hide();
+});
+var carrossel = document.querySelector("#carrossel");
+carrossel.addEventListener("mouseenter", function(){
+    $("#video").hide();
 });
