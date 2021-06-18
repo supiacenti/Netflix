48050aa72b8e145d8f0473824dda2cece9147600
Suellen Piacenti <84042162+supiacenti@users.noreply.github.com>
2021-06-18 20:19:20 -0300

    terceiro commit

--- Netflix/js/netflix.js
+++ Netflix/js/netflix.js
@@ -1,19 +1,26 @@
 $("#list").hide();
 
 $("#video").hide();
+
+$("#seta").hide();
+
 var d = document.querySelector("#destaque");
 var v = document.querySelector("#video");
+var l = document.querySelector("#l");
 var vc = document.getElementsByClassName("video");
-d.addEventListener("mouseenter", function(){
+var html = document.querySelector("#html");
+
+$("#destaque").one("mouseenter", function(){
     $("#video").show();
     v.autoplay = true;
     v.load();
 });
-d.addEventListener("mouseleave", function(){
+$("#destaque").one("click", function(){
     $("#video").hide();
     v.autoplay = false;
     v.load();
 });
+
 var c = document.querySelector("#conf");
 var c2 = document.querySelector("#conf2");
 c.addEventListener("mouseenter", function(){
@@ -28,7 +35,13 @@ c2.addEventListener("mouseenter", function(){
 c2.addEventListener("click", function(){
     $("#list").hide();
 });
-var carrossel = document.querySelector("#carrossel");
-carrossel.addEventListener("mouseenter", function(){
-    $("#video").hide();
+
+var t1 = document.querySelector("#titulo1");
+t1.addEventListener("mouseenter", function(){
+    $("#seta").show();
 });
+t1.addEventListener("mouseleave", function(){
+    $("#seta").hide();
+});
+
+var carrossel = document.querySelector("#carrossel");
